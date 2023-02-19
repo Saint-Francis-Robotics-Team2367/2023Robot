752d06953d23b7c73f6af6b2f75d752f9cf5183c
ESCAPE THE MATRIX <ethananikathil@sfhs.com>
2023-02-18 16:16:46 -0800

    pull origin working again

--- module/include/FindPath.h
+++ module/include/FindPath.h
@@ -13,11 +13,18 @@ class FindPath{
         startTurn(startTurn), endTurn(endTurn), pathTurn(pathTurn) { 
         }
     };
+    // same
     int robotWidth = 30;
+
     Instruction optimalPath(int pointFromOrigin[2], double angleStart, double angleEnd);
     Instruction circlePath(int pointFromOrigin[2], double angleStart, double angleEnd);
+
+    void arcPath(double x, double y, double *left, double *right);
+    void arcPathRadius(double x, double y, double *r, double *t);
+    void arcPathDist(double r, double t, double *in, double *out);
+
+    private:
     double minturn(double, double);
     double newangle(double, double);
     double diamToPower(double);
-    private:
 };
--- module/src/FindPath.cpp
+++ module/src/FindPath.cpp
@@ -2,28 +2,9 @@
 
 FindPath::Instruction FindPath::optimalPath(int pointFromOrigin[2], double angleStart, double angleEnd){
     
+
+
     return FindPath::Instruction(0,0,0);
-}
-
-double FindPath::minturn(double start, double end){
-    double diff = abs(start-end);
-    if(diff > 1) diff = 2 - diff;
-
-    if((start+1 < end) || (start-1 < end < start)){
-        diff *= -1;
-    }
-
-    return diff;
-}
-
-double FindPath::newangle(double start, double turn){
-    double sum = start + turn + 1;
-    double total = (fmod(sum, 2)) - 1;
-    return total;
-}
-
-double FindPath::diamToPower(double radius){
-    return 1 - robotWidth/radius;
 }
 
 FindPath::Instruction FindPath::circlePath(int pointFromOrigin[2], double angleStart, double angleEnd){
@@ -54,4 +35,52 @@ FindPath::Instruction FindPath::circlePath(int pointFromOrigin[2], double angleS
 
     return FindPath::Instruction(turns[0],turns[1],diamToPower(dist/2));
 }
+
+void FindPath::arcPath(double x, double y, double *left, double *right){
+    double *r, *t, *in, *out;
+    arcPathRadius(x,y,r,t);
+    arcPathDist(*r,*t,in,out);
+
+    if (x<0){
+        *left = *in;
+        *right = *out;
+    }
+    else{
+        *left = *out;
+        *right = *in;
+    }
+}
+
+void FindPath::arcPathRadius(double x, double y, double *r, double *t){
+    *r = (y*y)/(2*x) + x/2;
+    *t = acos((*r - x)/(*r));
+}
+
+void FindPath::arcPathDist(double r, double t, double *in, double *out){
+    *in = t * (r - this->robotWidth);
+    *out = t * (r + this->robotWidth);
+}
+
+double FindPath::minturn(double start, double end){
+    double diff = abs(start-end);
+    if(diff > 1) diff = 2 - diff;
+
+    if((start+1 < end) || (start-1 < end < start)){
+        diff *= -1;
+    }
+
+    return diff;
+}
+
+double FindPath::newangle(double start, double turn){
+    double sum = start + turn + 1;
+    double total = (fmod(sum, 2)) - 1;
+    return total;
+}
+
+double FindPath::diamToPower(double radius){
+    return 1 - robotWidth/radius;
+}
+
+
 
