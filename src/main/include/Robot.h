03b20f6bd5c69034a1d68a7c0a58edef83a1cfc0
TheRoanster <rohan0mittal@gmail.com>
2023-02-25 12:53:16 -0800

    fixed spelling and added min

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -6,11 +6,7 @@
 
 #include <frc/TimedRobot.h>
 #include "frc/smartdashboard/Smartdashboard.h"
-#include "networktables/NetworkTable.h"
-#include "networktables/NetworkTableInstance.h"
-#include "networktables/NetworkTableEntry.h"
-#include "networktables/NetworkTableValue.h"
-#include "wpi/span.h"
+
 
 class Robot : public frc::TimedRobot {
 
@@ -31,22 +27,6 @@ class Robot : public frc::TimedRobot {
 
   void TestInit() override;
   void TestPeriodic() override;
-  std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
-  double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
-  double targetOffsetAngle_Vertical = table->GetNumber("ty",0.0);
-  double targetArea = table->GetNumber("ta",0.0);
-  double targetSkew = table->GetNumber("ts",0.0);
-// how many degrees back is your limelight rotated from perfectly vertical?
-  double limelightMountAngleDegrees = 25.0;
-// distance from the center of the Limelight lens to the floor
-  double limelightLensHeightInches = 20.0;
-// distance from the target to the floor
-  double goalHeightInches = 60.0;
-  double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-  double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
-//calculate distance
-  double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/tan(angleToGoalRadians);
-
 };
 
 
