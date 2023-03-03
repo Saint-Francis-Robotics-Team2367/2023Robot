e2fc4ef29dede952327067e7c0cc73126c0b3cc1
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2023-03-03 15:18:50 -0800

    angles

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,26 +8,52 @@
 // // All Module Includes
 #include "DriveBaseModule.h"
 
-DriveBaseModule drive;
+//DriveBaseModule drive;
 //moved instantiation to h file
 
-
+#include <networktables/NetworkTableInstance.h>
 
 void Robot::RobotInit()
 {
   //compRobotDrive.periodicInit();
 
   //need drive inits
-  drive.driveThread.detach(); 
+  //drive.driveThread.detach(); 
  
 }
 
 void Robot::RobotPeriodic()
 {
+  double px = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx",0.0);
+  double py = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty",0.0);
+  // double tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx",0.0);
+  // double tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx",0.0);
+
+  frc::SmartDashboard::PutNumber("TX", px);
+  frc::SmartDashboard::PutNumber("TY", py);
+
+  double nx = (1/160) * (px - 159.5);
+  double ny = (1/120) * (119.5 - py);
+
+  double horizontal_fov = 54;
+  double vertical_fov = 41;
+
+  double vpw = 2.0*tan(horizontal_fov/2);
+  double vph = 2.0*tan(vertical_fov/2);
+
+  double x = vpw/2 * nx;
+  double y = vph/2 * ny;
+
+  double ax = atan2(1,x);
+  double ay = atan2(1,y);
+
+  frc::SmartDashboard::PutNumber("AX", ax);
+  frc::SmartDashboard::PutNumber("AY", ay);
+
 }
 void Robot::AutonomousInit()
 {
-  drive.state = 'a';
+  //drive.state = 'a';
 }
 void Robot::AutonomousPeriodic()
 {
@@ -38,7 +64,7 @@ void Robot::AutonomousPeriodic()
 
 void Robot::TeleopInit()
 {
-  drive.state = 't'; //add codes in while loops to break if state change
+  //drive.state = 't'; //add codes in while loops to break if state change
 }
 
 void Robot::TeleopPeriodic()
@@ -47,14 +73,14 @@ void Robot::TeleopPeriodic()
 }
 
 void Robot::DisabledInit() {
-  drive.stopAuto = true;
+  //drive.stopAuto = true;
 }
 void Robot::DisabledPeriodic() {
 }
 
 void Robot::TestInit()
 {
-  drive.state = 'd';
+  //drive.state = 'd';
 }
 
 void Robot::TestPeriodic()
