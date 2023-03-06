ef8ee24869c037c4dfd2a9c7960f1e3674b6d83a
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2023-03-06 15:46:27 -0800

    tuning

--- module/include/ElevatorModule.h
+++ module/include/ElevatorModule.h
@@ -7,7 +7,7 @@
 
 class ElevatorModule {
     int run_counter = 0;
-    double slowCoefficient = 4;
+    double slowCoefficient = 1;
     public:
     //Conversion Factors:
     double pitch_diameter = 1.273;
@@ -16,7 +16,7 @@ class ElevatorModule {
 
     //Mech/Electronics Setup
    // int m_ID = 10;
-    int m_ID = ElevatorID;
+    int m_ID = 13;
     
     rev::CANSparkMax* elevatorMotor = new rev::CANSparkMax(m_ID, rev::CANSparkMax::MotorType::kBrushless);
     rev::SparkMaxRelativeEncoder enc = elevatorMotor->GetEncoder();
@@ -26,14 +26,14 @@ class ElevatorModule {
 
     //constants
     double kElevatorMinHeight = 0.0;
-    double kElevatorMaxHeight = 47; //inches
+    double kElevatorMaxHeight = 29; //inches
 
 
     //no feedforward in this case...
-    double pDown = 0.8;
+    double pDown = 0.05;
     double dDown = 0.0;
 
-    double pUp = 1.0; 
+    double pUp = 0.1; 
     double dUp = 0.0;
 
     bool oneRun = false;
--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,60 +5,82 @@
 #include "Robot.h"
 #include <frc2/command/PIDCommand.h>
 #include <iostream>
+#include <frc/XboxController.h>
 
 // // All Module Includes
-#include "DriveBaseModule.h"
+//#include "DriveBaseModule.h"
+#include "ScaraArmModule.h"
+#include "ElevatorModule.h"
 
-DriveBaseModule drive;
-
+//DriveBaseModule drive;
+ScaraArmModule arm = ScaraArmModule();
+ElevatorModule elev = ElevatorModule(13);
+frc::XboxController* ctr = new frc::XboxController(0);
 //cpr number pulses (4096) per rev, 70 to 1 / 360
+
+double arm_startX = arm.innerSize + arm.outterSize;
+double arm_startY = 0;
 
 void Robot::RobotInit()
 {
-  /*
   arm.ArmInit();
   arm.innerPID.SetOutputRange(-0.2, 0.2);
   arm.outterPID.SetOutputRange(-0.2, 0.2);
-  frc::SmartDashboard::PutNumber("x", arm.innerSize);
-  frc::SmartDashboard::PutNumber("y", arm.outterSize);
   elev.Init();
+  frc::SmartDashboard::PutNumber("X_Calc", arm_startX);
+  frc::SmartDashboard::PutNumber("Y_Calc", arm_startY);
+  /*
   //compRobotDrive.periodicInit();
   */
   //need drive inits
-  drive.driveThread.detach(); 
+  //drive.driveThread.detach(); 
  
 }
 
 void Robot::RobotPeriodic()
 {
+  frc::SmartDashboard::PutNumber("Voltage", elev.elevatorMotor->GetBusVoltage());
+  frc::SmartDashboard::PutNumber("InnerAngle", arm.inner_enc.GetPosition());
+  frc::SmartDashboard::PutNumber("OutterAngle", arm.outter_enc.GetPosition());
+  
 }
 void Robot::AutonomousInit()
 {
-  drive.state = 'a';
+  // double x = frc::SmartDashboard::GetNumber("X_Calc", arm_startX);
+  // double y = frc::SmartDashboard::GetNumber("Y_Calc", arm_startY);
+  // arm.movetoXY(x, y);
+  
+
+  //drive.state = 'a';
 }
 void Robot::AutonomousPeriodic()
 {
+  elev.AutoPeriodic();
 }
 
 
 void Robot::TeleopInit()
 {
-  drive.state = 't'; //add codes in while loops to break if state change
+  //drive.state = 't'; //add codes in while loops to break if state change
 }
 
 void Robot::TeleopPeriodic()
 {
+  arm.inner->Set(ctr->GetRightX() / 5);
+  arm.outter->Set(ctr->GetLeftX() / 5);
+  elev.TeleopPeriodic(ctr->GetLeftTriggerAxis(), ctr->GetRightTriggerAxis());
+  //elev.elevatorMotor->Set(ctr->GetLeftTriggerAxis() - ctr->GetRightTriggerAxis());
 }
 
 void Robot::DisabledInit() {
-  drive.stopAuto = true;
+  //drive.stopAuto = true;
 }
 void Robot::DisabledPeriodic() {
 }
 
 void Robot::TestInit()
 {
-  drive.state = 'd';
+  //drive.state = 'd';
 }
 
 void Robot::TestPeriodic()
