00bcf03edeebd771a0677ffcd0c43fd0d1b8a4d5
Nitish Gourishetty <nitishgourishetty@gmail.com>
2023-03-18 15:45:34 -0700

    stages rdy

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -11,48 +11,39 @@
 #include "ElevatorModule.h"
 #include <frc/XboxController.h>
 
-// frc::XboxController* ctr = new frc::XboxController(0);
-// frc::XboxController* ctr2 = new frc::XboxController(1);
+frc::XboxController* ctr = new frc::XboxController(0);
+frc::XboxController* ctr2 = new frc::XboxController(1);
 
-// ScaraArmModule arm(ctr2);
+ScaraArmModule arm(ctr, ctr2);
 DriveBaseModule drive;
-// ElevatorModule elev(ctr);
-Limelight ll; 
+ElevatorModule elev(ctr, ctr2);
+
 
 //cpr number pulses (4096) per rev, 70 to 1 / 360
 
 void Robot::RobotInit()
 {
   drive.driveThread.detach(); 
-  // arm.scaraArmThread.detach();
-  // elev.elevatorThread.detach();
+  arm.scaraArmThread.detach();
+  elev.elevatorThread.detach();
 }
 
 void Robot::RobotPeriodic() {
-  //Get TargetPose Robot Space:
-  // std::vector<double> targetPose = ll.getTargetPoseRobotSpace();
-  // Limelight::Point targetXY = ll.getTargetXY(targetPose.at(0) * 39.37, targetPose.at(2) * 39.37, targetPose.at(4), Limelight::bottomLeftPole); // X, Y, yaw, poleID
-  // frc::SmartDashboard::PutNumber("TapeX", targetXY.x);
-  // frc::SmartDashboard::PutNumber("TapeY", targetXY.y);
-  // frc::SmartDashboard::PutNumber("Detected?", ll.getTargetDetected());
 
-  //ll.switchToPipeline(2);
-  //frc::SmartDashboard::PutNumber("LL Pipeline", ll.getPipeline());
-  drive.autoBalance();
 }
 
 void Robot::AutonomousInit() {
-  // arm.state = 'a';
+  arm.state = 'a';
   drive.state = 'a';
-  // elev.state = 'a';
+  elev.state = 'a';
 }
 void Robot::AutonomousPeriodic() {}
 
 
 void Robot::TeleopInit() {
-  // arm.state = 't';
+  arm.state = 't';
   drive.state = 't';
-  // elev.state = 't';
+  elev.state = 't';
 }
 
 void Robot::TeleopPeriodic() {}
