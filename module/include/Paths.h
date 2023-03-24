4079904aa911d495ca622cd5cdcee8f83f98b72e
Nitish Gourishetty <nitishgourishetty@gmail.com>
2023-03-24 01:11:20 -0700

    temp

---
+++ module/include/Paths.h
@@ -0,0 +1,56 @@
+#include <frc/SmartDashboard/SendableChooser.h>
+#include <string> 
+
+#ifndef Paths.h
+#define Paths.h
+
+enum autoPathType
+{
+    turn = 't',
+    straight = 's', 
+    elev = 'e',
+    arm = 'a'
+};
+
+class autoPath {
+    public:
+        autoPathType action; 
+        float dis; 
+        float angle; 
+        float radius; 
+        bool keepVelocity;
+        bool motionProfiling; 
+        int setpoint; 
+        double arm_x; 
+        double arm_y; 
+
+        autoPath(autoPathType type){
+            action = type;
+        };
+
+        void register_turn(float ang, float r=0, bool keepV=false){
+            keepVelocity = keepV; 
+            radius = r;
+            angle = ang; 
+        };
+
+        void register_straight(int d, bool keepV=false){
+            keepVelocity = keepV; 
+            dis = d; 
+        };
+
+        void register_elev(int point){
+            setpoint = point; 
+        };
+
+        void register_arm(double x, double y){
+            arm_x = x; 
+            arm_y = y; 
+        };
+    private:
+};
+
+#endif
+
+
+
